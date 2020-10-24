21d1c326ea90dc4b180d8c6245082a27a0f51d01
Nikolas Lenning <nikolaslenning@gmail.com>
2020-10-24 14:47:15 -0600

    updated connection.js w/ jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
-var Sequelize = require("sequelize");
+var mysql = require("mysql");
+// const password = require('../password');
 
-// Creates mySQL connection using Sequelize, the empty string in the third argument spot is our password.
-var sequelize = new Sequelize("sequelize_library", "root", "", {
-  host: "localhost",
-  port: 3306,
-  dialect: "mysql",
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db"
+  });
+}
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
   }
+  console.log("connected as id " + connection.threadId);
 });
 
-// Exports the connection for other files to use
-module.exports = sequelize;
+module.exports = connection;
