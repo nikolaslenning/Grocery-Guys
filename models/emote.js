4b81b6b440f36d0704c84370798c81a086704310
Nikolas Lenning <nikolaslenning@gmail.com>
2020-10-25 12:15:19 -0600

    transfered login verification, created emote model, associated w/ user

---
+++ models/emote.js
@@ -0,0 +1,56 @@
+
+module.exports = function(sequelize, DataTypes) {
+  var Emote = sequelize.define("Emote", {
+    url: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isUrl: true
+      }
+    },
+    anger: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    contempt: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    disgust: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    fear: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    happiness: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    neutral: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    sadness: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    surprise: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  });
+
+  Emote.associate = function(models) {
+    // We're saying that a Emote should belong to an Author
+    // A Emote can't be created without an Author due to the foreign key constraint
+    Emote.belongsTo(models.User, {
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return Emote;
+};
