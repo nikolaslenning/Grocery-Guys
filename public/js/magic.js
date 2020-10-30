988501bdcf7aa2002b202239cb8249adecf48bc9
Nikolas Lenning <nikolaslenning@gmail.com>
2020-10-29 21:30:30 -0600

    implemted handlebars delete button w/ delete route

--- public/js/magic.js
+++ public/js/magic.js
@@ -216,4 +216,21 @@ $("document").ready(function () {
   }
 
   );
+  // Code That controls delete button in index.handlebars
+  $(".delplan").on("click", function(event) {
+    // Get the ID from the button.
+    // This is shorthand for $(this).attr("data-planid")
+    var id = $(this).data("planid");
+
+    // Send the DELETE request.
+    $.ajax("/api/emotes/" + id, {
+      type: "DELETE"
+    }).then(
+      function() {
+        console.log("deleted id ", id);
+        // Reload the page to get the updated list
+        location.reload();
+      }
+    );
+  });
 });
