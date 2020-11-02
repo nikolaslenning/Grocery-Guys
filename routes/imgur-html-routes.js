7f35b827b6120162b9f9704ba61a6769cb9acc77
Nikolas Lenning <nikolaslenning@gmail.com>
2020-11-02 14:55:50 -0700

    img upload post route

--- routes/imgur-html-routes.js
+++ routes/imgur-html-routes.js
@@ -1,3 +1,7 @@
+const multer = require('multer');
+const upload = multer({dest: __dirname + '/uploads/images'});
+
+//https://medium.com/@nitinpatel_20236/image-upload-via-nodejs-server-3fe7d3faa642
 app.post('/upload', upload.single('photo'), (req, res) => {
   if (req.file) {
     res.json(req.file);
