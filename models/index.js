f76b0ee46010fc61801a0368b908f278f3be91ba
Nikolas Lenning <nikolaslenning@gmail.com>
2020-11-22 17:34:18 -0700

    removed commented out config variable

--- models/index.js
+++ models/index.js
@@ -7,7 +7,6 @@ const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/config.js')[env];
 // const config = require(__dirname + '/../config/config.json')[env];
-// const config = path.resolve('./config', 'config.js');
 const db = {};
 
 let sequelize;
